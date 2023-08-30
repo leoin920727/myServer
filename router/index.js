8d962b490da211556b72f84587dcaea9df52b51c
Leo <leoin920727@gmail.com>
2023-08-30 21:30:37 +0800

    1

--- router/index.js
+++ router/index.js
@@ -1,33 +1,41 @@
 var express = require("express");
-var cors = require('cors');
 var index = express.Router();
 
 index.get("/", function (req, res) {
-    res.send("可以用了");
-})
+  res.send("可以用了");
+});
 
-index.get("/orderlist", cors(), function (req, res) {
-    const data = [
-        {
-            "orderNumber": "C0021",
-            "memberId": "M01242351",
-            "orderDate": "13/08/23",
-            "weekOfTimes": "2",
-            "weekOfAmount": "62",
-            "price": "1200",
-            "orderStatus": "0"
-        },
-        {
-            "orderNumber": "C0020",
-            "memberId": "M01242351",
-            "orderDate": "13/08/23",
-            "weekOfTimes": "2",
-            "weekOfAmount": "62",
-            "price": "1200",
-            "orderStatus": "99"
-        }
-    ]
-    res.send(data)
-})
+index.get("/orderlist", function (req, res) {
+  const data = [
+    {
+      orderNumber: "C0021",
+      memberId: "M01242351",
+      orderDate: "13/08/23",
+      weekOfTimes: "2",
+      weekOfAmount: "62",
+      price: "1200",
+      orderStatus: "0",
+    },
+    {
+      orderNumber: "C0020",
+      memberId: "M01242351",
+      orderDate: "13/08/23",
+      weekOfTimes: "2",
+      weekOfAmount: "62",
+      price: "1200",
+      orderStatus: "1",
+    },
+    {
+      orderNumber: "C0020",
+      memberId: "M01242351",
+      orderDate: "13/08/23",
+      weekOfTimes: "2",
+      weekOfAmount: "62",
+      price: "1200",
+      orderStatus: "2",
+    },
+  ];
+  res.send(data);
+});
 
 module.exports = index;
