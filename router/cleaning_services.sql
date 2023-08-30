b4713d3813866f9dd7fcd34c33561240117df5c7
wayne <wayne362066@kimo.com>
2023-08-30 16:30:45 +0800

    wayne

--- router/cleaning_services.sql
+++ router/cleaning_services.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2023-08-30 08:41:54
+-- 產生時間： 2023-08-30 10:29:49
 -- 伺服器版本： 10.4.28-MariaDB
 -- PHP 版本： 8.2.4
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- 資料庫： `cleaning_services`
 --
+CREATE DATABASE IF NOT EXISTS `cleaning_services` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `cleaning_services`;
 
 -- --------------------------------------------------------
 
