a4616bf044706d30b798d00bbafd2cff31782c13
Leo <leoin920727@gmail.com>
2023-09-05 00:46:21 +0800

    updata router

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require("express");
 var index = require("./router/index");
+var dashboard = require("./router/dashboard");
 const bodyParser = require("body-parser");
 var cors = require("cors");
 var app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use("/", dashboard);
 app.use("/", index);
 
 app.listen(4107, function () {
---
+++ router/dashboard.js
@@ -0,0 +1,187 @@
+const express = require("express");
+var db = require("../db");
+const dashboard = express.Router();
+
+// 後台訂單資料
+dashboard.get("/orderlist", function (req, res) {
+  const data = [
+    {
+      orderNumber: "C0021",
+      memberId: "M351",
+      orderDate: "13/08/23",
+      weekOfTimes: "2",
+      weekOfAmount: "62",
+      price: "1200",
+      orderStatus: "0",
+    },
+    {
+      orderNumber: "C0020",
+      memberId: "M0121",
+      orderDate: "13/08/23",
+      weekOfTimes: "2",
+      weekOfAmount: "62",
+      price: "1200",
+      orderStatus: "1",
+    },
+    {
+      orderNumber: "C0020",
+      memberId: "M0124",
+      orderDate: "13/08/23",
+      weekOfTimes: "2",
+      weekOfAmount: "62",
+      price: "1200",
+      orderStatus: "2",
+    },
+  ];
+  res.send(data);
+});
+
+// 後台訂單資料(詳細)
+dashboard.get("/AdminOrder/:order", function (req, res) {
+  const orderNumber = req.params.orderNumber;
+  const data = {
+    IDnum: "TXXXXXXXXX",
+    staffName: "王阿花", //指定人員
+    staffId: "M0020", //員工編號
+    time_W: "星期三", //服務星期
+    time_T: "1300", //服務時段
+    memberName: "鍾秋節", //姓名
+    memberId: "M351", //會員編號
+    phone: "0912345667", //手機
+    email: "123@gmail", //電子信箱
+    city: "台中市",
+    adreess: "黎明路二段658號", //清掃地址 區域
+    common: "客廳的花瓶不要移動，那花瓶要十二萬，清理時要小心點。", //訂單備註
+    pay: "VISA", //付款方式
+    weekOfTimes: 2, //選擇服務週數
+    orderDate: "13/08/23", //服務開始日期
+    orderNumber: "C0021", //訂單編號
+    orderStatus: 0, //訂單狀態
+    weekOfAmount: 6, //服務次數
+    finish: 2, //完成次數
+    price: 1200, //訂單金額
+    imgUrl: "/images/vase.png",
+    staffPhone: "0912345678",
+    staffEmail: "123@gmail.com",
+  };
+  res.send(data);
+});
+
+// 後台訂單更新
+dashboard.put("/AdminOrder/updata", function (req, res) {
+  const requestData = req.body;
+  res.json({ message: "Data received successfully", data: requestData });
+});
+
+// 會員評分更新
+dashboard.put("/member/updata/:orderID", function (req, res) {
+  const requestData = req.body;
+  res.json({ message: "Data received successfully", data: requestData });
+});
+
+// 會員資料表
+dashboard.get("/dashboard/memberInfo", function (req, res) {
+  var sql = `SELECT * FROM userinfo`;
+  var data = [];
+  db.exec(sql, data, function (results, fields) {
+    res.send(results);
+  });
+});
+
+// 會員資料內容
+dashboard.get("/dashboard/PersonalInfo/:uid", function (req, res) {
+  const uid = req.params.uid;
+  var sql1 = `SELECT * FROM userinfo`;
+  var sql2 = `SELECT * FROM userinfo WHERE uid=?`;
+  var sql3 = `SELECT (whyblacklist) AS why FROM userinfo,blacklist WHERE userinfo.uid=blacklist.uid AND userinfo.uid=?`;
+  var data = [uid];
+  db.exec(sql1, [], function (number, fields) {
+    db.exec(sql2, data, function (results, fields) {
+      db.exec(sql3, data, function (why, fields) {
+        const ban = why.length === 0 ? " " : why;
+        const len = number.length;
+        res.send({ data: results, length: len, why: ban });
+      });
+    });
+  });
+});
+
+// 新增黑名單
+dashboard.put("/dashboard/PersonalInfo/blacklist/:uid", function (req, res) {
+  const uid = req.params.uid;
+  const why = req.body.why;
+  var sql1 = `UPDATE userinfo SET blacklist = 1 WHERE userinfo.uid = ?`;
+  var sql2 = `INSERT INTO blacklist (uid, whyblacklist) VALUES (?,?)`;
+  var data1 = [uid];
+  var data2 = [uid, why];
+  db.exec(sql1, data1, function (results2, fields) {
+    db.exec(sql2, data2, function (results, fields) {
+      res.send({ message: "success" });
+    });
+  });
+});
+
+// 更新黑名單
+dashboard.put(
+  "/dashboard/PersonalInfo/removeblacklist/:uid",
+  function (req, res) {
+    const uid = req.params.uid;
+    var sql = `UPDATE userinfo SET blacklist = 0 WHERE userinfo.uid =?`;
+    var data = [uid];
+    db.exec(sql, data, function (results, fields) {
+      res.send({ message: "success" });
+    });
+  }
+);
+
+// 刪除黑名單
+dashboard.delete(
+  "/dashboard/PersonalInfo/removeblacklist/:uid",
+  function (req, res) {
+    const uid = req.params.uid;
+    var sql = `DELETE FROM blacklist WHERE blacklist.uid=?`;
+    var data = [uid];
+    db.exec(sql, data, function (results, fields) {
+      res.send({ message: "success" });
+    });
+  }
+);
+
+// 員工資料表
+dashboard.get("/dashboard/StaffList", function (req, res) {
+  var sql = `SELECT * FROM employeeinfo`;
+  var data = [];
+  db.exec(sql, data, function (results, fields) {
+    res.send(results);
+  });
+});
+
+// 員工資料內容 //需要員工的PK數字
+dashboard.get("/dashboard/StaffList/:employeeid", function (req, res) {
+  const employeeid = req.params.employeeid;
+  var sql1 = `SELECT * FROM employeeinfo WHERE employeeid= ?`;
+  var sql2 = `SELECT COUNT(*) AS length FROM employeeinfo`;
+  var sql3 = `SELECT * FROM employeeinfo 
+    INNER JOIN evaluate ON employeeinfo.employeeid = evaluate.employeeid 
+    INNER JOIN orderlist ON evaluate.ornumber = orderlist.ornumber 
+    WHERE employeeinfo.employeeid= ?;`;
+  var data = [employeeid];
+  db.exec(sql1, data, function (results, fields) {
+    db.exec(sql2, data, function (length, fields) {
+      db.exec(sql3, data, function (list, fields) {
+        res.send({ data: results, length: length, list: list });
+      });
+    });
+  });
+});
+
+// 黑名單
+dashboard.get("/dashboard/blacklist", function (req, res) {
+  var sql = `SELECT * FROM userinfo,blacklist WHERE userinfo.uid=blacklist.uid;`;
+  var data = [];
+  db.exec(sql, data, function (results, fields) {
+    res.send(results);
+  });
+});
+
+module.exports = dashboard;
