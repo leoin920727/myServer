55857a45525eb99bf1beede6aacb96f2500c2afd
wayne <wayne362066@kimo.com>
2023-09-12 09:18:59 +0800

    wayne

--- router/book.js
+++ router/book.js
@@ -9,7 +9,7 @@ let myDBconn = mysql.createConnection({
   // port: "8889",
   port: "3306",
   user: "root",
-  password: "",
+  password: "root",
   database: "cleaning_services",
   multipleStatements: true,
 });
